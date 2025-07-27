3be9551446cbcdf181b3b6b6a176e12b8664f7ff
Krishna Yadav <yadavkrish2709@gmail.com>
2025-07-27 12:14:49 +0530

    Added service-worker.js to make it downloadable locally

---
+++ src/service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "scientific-calculator-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icon-192.png",
+  "/icon-512.png",
+];
+
+// Install event: Cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate event: Clean old caches if any
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+});
+
+// Fetch event: Serve from cache first, then network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
